1cadd81347c887aca87a8d73fd60de6caa0684d0
Heron Michanikus <heronmichanikus@gmail.com>
2018-07-07 18:24:19 +0300

    Получение имени БД

--- views/module4/main.php
+++ views/module4/main.php
@@ -11,3 +11,21 @@ $this->title = 'Информация о базе данных';
 <div class="container">
     <h2>Информация о базе данных</h2>   
 </div>
+
+
+<?php
+
+$db = new yii\db\Connection([
+			'dsn' => 'mysql:host=localhost;dbname=db_preview',
+			'username' => 'root',
+			'password' => '',
+			'charset' => 'utf8',
+		]);
+
+
+$posts = $db->createCommand('show databases');
+echo gettype($posts);
+
+?>
+
+
