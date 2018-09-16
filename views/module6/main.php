e021701aa26c51560f9384f3166ff35f1461838c
Heron Michanikus <heronmichanikus@gmail.com>
2018-09-17 00:18:57 +0300

    Получение статистических данных команды

--- views/module6/main.php
+++ views/module6/main.php
@@ -30,7 +30,37 @@ include(Yii::getAlias('@app/web/my_config/module6.php'));
         </div>
     </form>
     
-    <h3><?= $id_team?></h3>
+    <h3><?= $arr_team[$id_team]?></h3>
     
-    <p><?= printArray($result_query)?></p>
+    
+    
+    <div id='matchs-team1'>
+        <?php foreach($result_query as $val):?>
+        <?php
+            //установить часовой пояс по умолчанию
+            date_default_timezone_set('Europe/Rome');
+            //echo date_default_timezone_get();
+            if(trim($val['place']) == 'home'){
+                echo '<p>'.gmdate("d.m.Y",$val['date_match']).'  '.
+                     $arr_team[$val['id_team']].' - '.$val['rival']."  <code>".
+                     $val['puck_team'].':'.$val['puck_rival'].   
+                     '</code></p>';    
+            }else{
+                echo '<p>'.gmdate("d.m.Y",$val['date_match']).'  '.
+                     $val['rival'].' - '.$arr_team[$val['id_team']]."  <code>".
+                     $val['puck_rival'].':'.$val['puck_team'].
+                     '</code></p>';
+                
+            }
+        ?>
+        <?php endforeach?>
+    </div>
+    
+    
+    <p><?php /*printArray($result_query)*/?></p>
+    
+    
+    
+    
+    
 
