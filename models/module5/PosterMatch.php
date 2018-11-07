c2d12e2812e4c41365bcae3079a0c92a677521ba
Heron Michanikus <heronmichanikus@gmail.com>
2018-11-07 23:07:14 +0300

    Постер матча

--- models/module5/PosterMatch.php
+++ models/module5/PosterMatch.php
@@ -97,12 +97,96 @@ class PosterMatch extends Model
         $this->all_data['city']=$data_request['city'];                
         // создание подключения к БД
         $this->id_connect_DB = Yii::$app->db_khl_stat_2018;
+        // название команд
+        $this->all_data['name_team_1']=$this->id_connect_DB->createCommand('SELECT name FROM table_conf WHERE id_team='.$this->all_data['id_team1'])->queryAll()[0]['name'];
+        $this->all_data['name_team_2']=$this->id_connect_DB->createCommand('SELECT name FROM table_conf WHERE id_team='.$this->all_data['id_team2'])->queryAll()[0]['name']; 
+        // занимаемое место в таблице
+        $this->all_data['place_team_1']=$this->id_connect_DB->createCommand('SELECT place FROM table_conf WHERE id_team='.$this->all_data['id_team1'])->queryAll()[0]['place'];
+        $this->all_data['place_team_2']=$this->id_connect_DB->createCommand('SELECT place FROM table_conf WHERE id_team='.$this->all_data['id_team2'])->queryAll()[0]['place']; 
+        // конференция в которой играет команда
+        $this->all_data['conf_1']=$this->id_connect_DB->createCommand('SELECT conf FROM table_conf WHERE id_team='.$this->all_data['id_team1'])->queryAll()[0]['conf'];
+        $this->all_data['conf_2']=$this->id_connect_DB->createCommand('SELECT conf FROM table_conf WHERE id_team='.$this->all_data['id_team2'])->queryAll()[0]['conf'];
+         
+         
+         $this->get_value();
+         $this->position();
+         
     }
     
 
     // получения данных из БД
     function get_value(){
-       
+        //Количестов побед
+        $query_1='SELECT clear_wins + ot_wins + b_wins FROM table_conf WHERE id_team='.$this->all_data['id_team1'];
+        $query_2='SELECT clear_wins + ot_wins + b_wins FROM table_conf WHERE id_team='.$this->all_data['id_team2'];
+        $this->all_data['wins_1']=$this->id_connect_DB->createCommand($query_1)->queryScalar();
+        $this->all_data['wins_2']=$this->id_connect_DB->createCommand($query_2)->queryScalar();
+        // Количестов поражений
+        $query_1='SELECT clear_defeat+ot_defeat+b_defeat FROM table_conf WHERE id_team='.$this->all_data['id_team1'];
+        $query_2='SELECT clear_defeat+ot_defeat+b_defeat FROM table_conf WHERE id_team='.$this->all_data['id_team2'];
+        $this->all_data['defeats_1']=$this->id_connect_DB->createCommand($query_1)->queryScalar();
+        $this->all_data['defeats_2']=$this->id_connect_DB->createCommand($query_2)->queryScalar();
+        // 
+        
+        
+        
+        /*
+        <li>Количестов побед (все)</li>
+            <li>Количестов поражений (все)</li>
+            <li>Место в конференции - с соседями по таблице (место, клуб, очки)</li>
+            <li>Процент набранных очков</li>
+            <li>Количество заброшенных шайб</li>
+            <li>Количество проведенных бросков</li>
+            <li>Процент реализации бросков</li>
+            <li>Количество полученного большинства</li>
+            <li>Количество реализованного большинства</li>
+            <li>Процент реализации большинства</li>
+            <li>Количество полученного меньшинства</li>
+            <li>Количество реализованного большинства противником</li>
+            <li>Процент реализации большинства противником</li>
+            <li>Силовые приемы</li>
+            <li>Блокированные броски</li>
+        <li>Количество вбрасываний</li>
+       */
+    }
+    
+    
+    
+    // определение положения команд в турнирной таблице
+    function position(){
+        /*
+        все данные заносим в массив
+
+        all_data['position_1'][0]['place']['team']['scores']
+        all_data['position_1'][1]['place']['team']['scores']
+        all_data['position_1'][2]['place']['team']['scores']
+        
+        */
+        
+        // если первая команда занимает первое место
+        if($this->all_data['place_team_1'] == 1){
+            for($i=0; $i<=2; $i++){
+                // место
+                $this->all_data['position_1'][$i]['place']=$i+1;
+                // имя команды
+                $this->all_data['position_1'][$i]['team']=$this->id_connect_DB->createCommand('SELECT name FROM table_conf WHERE place='.($i+1).' AND conf="'.$this->all_data['conf_1'].'"')->queryAll()[0]['name'];
+                // набранные очки
+                $this->all_data['position_1'][$i]['scores']=$this->id_connect_DB->createCommand('SELECT scores FROM table_conf WHERE place='.($i+1).' AND conf="'.$this->all_data['conf_1'].'"')->queryAll()[0]['scores'];    
+            }   
+        }
+        // если вторая команда занимает первое место
+        if($this->all_data['place_team_2'] == 1){
+            for($i=0; $i<=2; $i++){
+                // место
+                $this->all_data['position_2'][$i]['place']=$i+1;
+                // имя команды
+                $this->all_data['position_2'][$i]['team']=$this->id_connect_DB->createCommand('SELECT name FROM table_conf WHERE place='.($i+1).' AND conf="'.$this->all_data['conf_2'].'"')->queryAll()[0]['name'];
+                // набранные очки
+                $this->all_data['position_2'][$i]['scores']=$this->id_connect_DB->createCommand('SELECT scores FROM table_conf WHERE place='.($i+1).' AND conf="'.$this->all_data['conf_2'].'"')->queryAll()[0]['scores'];    
+            }   
+        }
+//        
+        
     }
     
 
